0de920f1804300dd5ace8e45c5dd5e9950463174
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-10-05 18:16:24 -0400

    Update SquareMatrix.h

--- SquareMatrix.h
+++ SquareMatrix.h
@@ -13,14 +13,14 @@ class SquareMatrix
 	   SquareMatrix(SquareMatrix<T> && rhs);   // Move constructor
 	   SquareMatrix & operator= (const SquareMatrix<T> & rhs);  // Copy assignment
 	   SquareMatrix & operator= (SquareMatrix<T> && rhs);   // Move assignment
-	   bool operator==(const SquareMatrix& A, const SquareMatrix& B); // Matrices A and B are considered equal if their sizes are equal and all elements of A match the corresponding elements of B.
-	   SquareMatrix operator+ (const SquareMatrix& A, const SquareMatrix& B); //The result of addition of two square matrices A and B
-	   void resize(T* new_size);  // Restroys all previous content of the matrix and releases all memory. Then it allocates new memory. New elements are not specified (can have any value)
+	   bool operator==(const SquareMatrix& rhs)const; // Matrices A and B are considered equal if their sizes are equal and all elements of A match the corresponding elements of B.
+	   SquareMatrix operator+ (const SquareMatrix& rhs)const; //The result of addition of two square matrices A and B
+	   void resize(int new_size);  // Restroys all previous content of the matrix and releases all memory. Then it allocates new memory. New elements are not specified (can have any value)
 	   const T& at(int row, int column) const;  // Returns a reference to the element in the given row and column
-	   int size() const;   // Returns the current size of the matrix
+	   int size();   // Returns the current size of the matrix
 	   
      private:
-	   int num_rows_, num_cols_;
+	   int size_;
 	   T* array_;
 };
 
@@ -28,8 +28,7 @@ class SquareMatrix
 template<class T>
 SquareMatrix<T>::SquareMatrix()
 {
-	num_rows_ = 0;
-	num_cols_ = 0;
+	size_ = 0;
 	array_ = nullptr;
 }
 
@@ -41,8 +40,7 @@ SquareMatrix<T>::~SquareMatrix(){};
 template<class T>
 SquareMatrix<T>::SquareMatrix(const SquareMatrix<T>& rhs)
 {	
-	num_rows_ = 0;
-	num_cols_ = 0;
+	size_ = 0;
 	array_ = nullptr;
 
 	*this = rhs;
@@ -53,8 +51,7 @@ template<class T>
 SquareMatrix<T>::SquareMatrix(SquareMatrix<T>&& rhs) : array_{rhs.array_}
 {
 	/**Initialize the new *this
-	num_rows_ = 0;
-	num_cols_ = 0;
+	size_ = 0;
 	array_ = nullptr;
 
 	// Shallow copy of other
@@ -64,23 +61,22 @@ SquareMatrix<T>::SquareMatrix(SquareMatrix<T>&& rhs) : array_{rhs.array_}
 
 /** copy assignment**/
 template<class T>
-SquareMatrix<T>::operator=(const SquareMatrix<T>& rhs)
+SquareMatrix<T>& SquareMatrix<T>::operator=(const SquareMatrix<T>& rhs)
 {	
 	// Get dimensions
-	/**num_rows_ = rhs.num_rows_;
-	num_cols_ = rhs.num_cols_;
+	/**size_ = rhs.size_;
 
 	// (Re)Initialize Array
-	array_ = new T*[num_rows_];
-	for (int i=0; i < num_rows_; i++)
+	array_ = new T*[size_];
+	for (int i=0; i < size_; i++)
 	{
-		array_[i] = new T[num_cols_];
+		array_[i] = new T[size_];
 	}
 
 	// Fill Array
-	for (int i=0; i < num_rows_; i++)
+	for (int i=0; i < size_; i++)
 	{
-		for (int j=0; j < num_cols_; j++)
+		for (int j=0; j < size_; j++)
 		{
 			array_[i][j] = rhs.array_[i][j];
 		}
@@ -95,15 +91,13 @@ SquareMatrix<T>::operator=(const SquareMatrix<T>& rhs)
 
 /** Move assignment **/
 template<class T>
-SquareMatrix<T>::SquareMatrix & operator= (SquareMatrix<T> && rhs)
+SquareMatrix<T>& SquareMatrix<T>::operator= (SquareMatrix<T> && rhs)
 {
-	num_rows_ = rhs.num_rows_;
-	num_cols_ = rhs.num_cols_;
+	size_ = rhs.size_;
 	array_ = rhs.array_;
 
 	// distroy the contents of rhs, and release memory
-	rhs.num_rows_ = 0;
-	rhs.num_cols_ = 0;
+	rhs.size_ = 0;
 	rhs.array_ = nullptr;
 	
 	return *this;
@@ -111,43 +105,58 @@ SquareMatrix<T>::SquareMatrix & operator= (SquareMatrix<T> && rhs)
 
 /**@return true if SquareMatrix A is equal to SquareMatrix B, otherwise is false**/
 template<class T>
-bool operator==(const SquareMatrix& A, const SquareMatrix& B)
+bool SquareMatrix<T> ::operator==(const SquareMatrix<T>& rhs)
 {
-	return (A.num_rows_ == B.num_rows_) && (A.num_cols_ == B.num_cols_) && (A[i][j] == B[i][j]);
+	if(size_ != rhs.size_)
+		return false;
+	for(int i=0; i<size_; i++)
+	{
+		for(int j=0; j<size_; j++)
+		{
+			if(array_[i][j] != rhs.array_[i][j])
+				return false;
+		}
+	}
+	return true;
 }
 
 /**@return third SquareMatrix which is the sum of SquareMatrix A and SquareMatrix B**/
-SquareMatrix operator+ (const SquareMatrix& A, const SquareMatrix& B)
+Template<class T>
+SquareMatrix<T> SquareMatrix<T>:: operator+ (const SquareMatrix<T>& rhs)
 {
-	SquareMatrix& S;
-	if(operator==(const SquareMatrix& A, const SquareMatrix& B))
-	{
-		SquareMatrix& S = SquareMatrix& A + SquareMatrix& B;
+	SquareMatrix<T> result(*this);
+	if(operator==(const SquareMatrix<T>& rhs)
+	{   
+		for (int i = 0; i < size_; i++)
+		{
+			for (int j = 0; j < size_; j++)
+			{
+				result.array_[i][j] += rhs.array_[i][j];
+			}
 	}
-	return SquareMatrix& S;
+	return result;
 }
 
 /**This function destroys all previous content of the matrix and releases all memory.
  Then it allocates new memory. New elements are not specified (can have any value).**/
-void resize(const T& new_size)
+template<class T>
+void SquareMatrix<T>::resize(const int new_size)
 {
-	for (int i = 0; i < num_rows_; i++)
+	for (int i = 0; i < size_; i++)
 	{
 		delete[] array_[i]; //This destroys all previous content of the matrix
 	}
 
 	delete[] array_;  //release memory
 	array_ = nullptr;
-	num_rows_ = 0;
-	num_cols_ = 0;	
+	size_ = 0;
 
-	break;
 }
 
 /**@ returns a reference to the element in the given row and column**/
 //If the suggested row or column doesn't exist, throw the std::out_of_range exception.
 template<class T>
-const T& at(int row, int col) const
+const T& SquareMatrix<T>::at(int row, int col) const
 {
 	return array_[row][col];
 }
@@ -155,9 +164,9 @@ const T& at(int row, int col) const
 /**@return the current size of square matrix**/
 /**the size 10 means that the matrix is 10X10 elements large. **/
 template<class T>
-int size() const
+int SquareMatrix<T>::size()
 {
-	return num_rows_;  //because it is a Square matrix, either num_rows_ or num_cols_ is fine
+	return size_;  
 }
 
 #endif
