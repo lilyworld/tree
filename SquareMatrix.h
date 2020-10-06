ed54dcb51c36e57fbbeed79c20bc9c1a23e77b07
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-10-05 20:50:11 -0400

    Update SquareMatrix.h

--- SquareMatrix.h
+++ SquareMatrix.h
@@ -17,7 +17,7 @@ class SquareMatrix
 	   SquareMatrix operator+ (const SquareMatrix<T>& rhs)const; //The result of addition of two square matrices A and B
 	   void resize(int new_size);  // Restroys all previous content of the matrix and releases all memory. Then it allocates new memory. New elements are not specified (can have any value)
 	   const T& at(int row, int column) const;  // Returns a reference to the element in the given row and column
-	   int size();   // Returns the current size of the matrix
+	   int size()const;   // Returns the current size of the matrix
 	   
      private:
 	   int size_;
@@ -105,7 +105,7 @@ SquareMatrix<T>& SquareMatrix<T>::operator= (SquareMatrix<T> && rhs)
 
 /**@return true if SquareMatrix A is equal to SquareMatrix B, otherwise is false**/
 template<class T>
-bool SquareMatrix<T>::operator==(const SquareMatrix<T>& rhs)
+bool SquareMatrix<T>::operator==(const SquareMatrix<T>& rhs) const
 {
 	if(size_ != rhs.size_)
 		return false;
@@ -122,7 +122,7 @@ bool SquareMatrix<T>::operator==(const SquareMatrix<T>& rhs)
 
 /**@return third SquareMatrix which is the sum of SquareMatrix A and SquareMatrix B**/
 template<class T>
-SquareMatrix<T> SquareMatrix<T>:: operator+ (const SquareMatrix<T>& rhs)
+SquareMatrix<T> SquareMatrix<T>:: operator+ (const SquareMatrix<T>& rhs) const
 {
 	SquareMatrix<T> result(*this);
 	if(operator==(const SquareMatrix<T>& rhs)
@@ -158,13 +158,16 @@ void SquareMatrix<T>::resize(const int new_size)
 template<class T>
 const T& SquareMatrix<T>::at(int row, int col) const
 {
-	return array_[row][col];
+	if(row > size_ || col > size_)
+		cout<< "out_of_range exception" << endl;
+	else
+		return array_[row][col];
 }
 
 /**@return the current size of square matrix**/
 /**the size 10 means that the matrix is 10X10 elements large. **/
 template<class T>
-int SquareMatrix<T>::size()
+int SquareMatrix<T>::size() const
 {
 	return size_;  
 }
