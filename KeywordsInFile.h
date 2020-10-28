854995c85348d6d28879c3c978a02a2727d1098b
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-10-27 22:56:26 -0400

    Update KeywordsInFile.h

--- KeywordsInFile.h
+++ KeywordsInFile.h
@@ -1,1 +1,17 @@
 
+#include<iostream>
+#include<string>
+using namespace std;
+
+class KeywordsInFile
+{
+  pubilc:
+    KeywordsInFile(); // default constructor
+    KeywordsInFile(filename_with_keywords, filename_with_text);
+    bool KeywordFound(string keyword);
+    int KeywordInLine(string keyword, int line_number);
+    int TotalOccurrences(string keyword);
+    void operator<<(KeywordsInFile obj);
+  
+  private:
+    
