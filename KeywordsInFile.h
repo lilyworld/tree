204ef6e6e62822eddbe00f62c91fe9e18d53a059
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-10-27 23:44:28 -0400

    Update KeywordsInFile.h

--- KeywordsInFile.h
+++ KeywordsInFile.h
@@ -2,20 +2,35 @@
 #define	KEYWORDSINFILE_H
 #include<iostream>
 #include<string>
-using namespace std;
+#include<vector>
+#include<unordered_map>
 
 class KeywordsInFile
 {
   pubilc:
-    KeywordsInFile(); // default constructor
-    KeywordsInFile(filename_with_keywords, filename_with_text);
-    bool KeywordFound(string keyword);
-    int KeywordInLine(string keyword, int line_number);
-    int TotalOccurrences(string keyword);
+    KeywordsInFile() = delete; // default constructor
+   
+    /** Parameterized constructor
+     @param filename_with_keywords: is a name of a plain text file that contains the list of keywords.
+     @param filename_with_text: is a name of a plain text file that contains the text where the keywords must be found.  **/
+    KeywordsInFile(const& std::string filename_with_keywords, const& std::string filename_with_text);
+  
+    /**@returns true if the specified keyword was found in the text. Returns false otherwise. **/
+    bool KeywordFound(std::string keyword);
+  
+    /**@returns the number of times the specified keyword was seen in  the specified line of the text. **/
+    int KeywordInLine(std::string keyword, int line_number);
+  
+    /**@returns the total number of the occurrences of the given keyword in the text. **/
+    int TotalOccurrences(std::string keyword);
+  
+    /** that allows you to use object of your class with cout. **/
     void operator<<(KeywordsInFile obj);
   
   private:
-    
+    std::vector<std::string> Keywords;
+    std::unordered_map<std::string,int> store;
+    std::vector<vector<std::string>> line_has_key;
   
 };
 
