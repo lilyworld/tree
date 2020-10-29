8417623a3b6ca838f9b2f4774de477ab0a8e3738
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-10-28 23:07:11 -0400

    Update KeywordsInFile.h

--- KeywordsInFile.h
+++ KeywordsInFile.h
@@ -1,19 +1,19 @@
 #ifndef KEYWORDSINFILE_H
 #define	KEYWORDSINFILE_H
-#include<iostream>
-#include<string>
-#include<vector>
-#include<unordered_map>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
 
 class KeywordsInFile
 {
-  pubilc:
+  public:
     KeywordsInFile() = delete; // default constructor
    
     /** Parameterized constructor
      @param filename_with_keywords: is a name of a plain text file that contains the list of keywords.
      @param filename_with_text: is a name of a plain text file that contains the text where the keywords must be found.  **/
-    KeywordsInFile(const& std::string filename_with_keywords, const& std::string filename_with_text);
+    KeywordsInFile(std::const& string filename_with_keywords, std::const& string filename_with_text);
   
     /**@returns true if the specified keyword was found in the text. Returns false otherwise. **/
     bool KeywordFound(std::string keyword);
@@ -28,9 +28,9 @@ class KeywordsInFile
     void operator<<(KeywordsInFile obj);
   
  private:
-    unordered_map<string, int> keywords;;  // 'keywords' will store all keywords in filename_with_keywords
-    unordered_map<string, int> numOfKeyWord;  // 'numOfKeyWord' will keep count of all keywords.
-    vector<vector<string>> wordsInLine;    // 'wordsInLine' will keep count of words in a particular line.
+    std::unordered_map<std::string, int> keywords;  // 'keywords' will store all keywords in filename_with_keywords
+    std::unordered_map<std::string, int> numOfKeyWord;  // 'numOfKeyWord' will keep count of all keywords.
+    std::vector<std::vector<std::string>> wordsInLine;    // 'wordsInLine' will keep count of words in a particular line.
   
 };
 
