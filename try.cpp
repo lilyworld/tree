21e6b56e8ff7417375ba43ef0f9d9ba09c3e7843
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-10-29 19:41:48 -0400

    Update try.cpp

--- try.cpp
+++ try.cpp
@@ -108,8 +108,14 @@ void KeywordsInFile::operator<<(KeywordsInFile obj)
 
 int main()
 {
-    std::string keyFile = "keyword.txt";
-    std::string txtFile = "text_file.txt";
+     // 'text_file' will take user input for text file.
+   // 'keywords_file' wwill take user input for keyword file.
+   string text_file, keywords_file;
+   cout << "Enter the file name of file containing keywords: ";
+   cin >> keywords_file;
+   cout << "Enter the file name of simple text file: ";
+   cin >> text_file;
+	
     KeywordsInFile file(keyFile, txtFile);
 
     //should return 0
