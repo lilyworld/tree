c23e36b7ebeb66adf37a4d138fff8ad539014a80
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-10-29 18:59:20 -0400

    Update try.cpp

--- try.cpp
+++ try.cpp
@@ -102,3 +102,25 @@ void KeywordsInFile::operator<<(KeywordsInFile obj)
         cout<<endl;
 }
 **/
+
+int main()
+{
+    std::string keyFile = "keyword.txt";
+    std::string txtFile = "text_file.txt";
+    KeywordsInFile file(keyFile, txtFile);
+
+    //should return 0
+    std::cout << "KEYWORDFOUND()" << std::endl;
+    std::cout << file.KeywordFound("night") <<std::endl;
+
+    //should return 6
+    std::cout << "TOTALOCCURENCES()" << std::endl;
+    std::cout << file.TotalOccurrences("lazy") << std::endl;
+
+    //should print "keyword:occurences" for each keyword
+    //std::cout << "OS>>" << std::endl;
+    //std::cout << file << std::endl;
+
+    //should print 3 for line 3 (4 in total text file)
+    std::cout << file.KeywordInLine("lol", 3) << std::endl;
+}
