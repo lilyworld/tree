3f460c8008dc1d20079575edf9ccf092de3d397d
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-11-18 21:08:18 -0500

    Create Field.h

---
+++ Field.h
@@ -0,0 +1,18 @@
+#ifdefine
+#include <iostream>
+#include <vector>
+
+class Field
+{
+  public:
+    Field(const vector<vector<int>> & )
+    Field(vector<vector<int>> && )
+    int Weight(int x1, int y1, int x2, int y2)
+    
+    //This method computes the cost of the cheapest path from top-left corner of the field to the bottom-right corner.
+    //The only possible moves are right and down.
+    int PathCost() 
+    
+ };
+
+
