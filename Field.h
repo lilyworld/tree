5fa7f375b583d0f027169655b8aeef4a1568a7a7
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-11-18 21:11:08 -0500

    Update Field.h

--- Field.h
+++ Field.h
@@ -1,18 +1,21 @@
-#ifdefine
+#ifndef FIELD_H
+#define	FIELD_H
 #include <iostream>
 #include <vector>
 
 class Field
 {
   public:
-    Field(const vector<vector<int>> & )
-    Field(vector<vector<int>> && )
-    int Weight(int x1, int y1, int x2, int y2)
+    Field(const vector<vector<int>> & element);
+    Field(vector<vector<int>> && element);
+    int Weight(int x1, int y1, int x2, int y2);
     
     //This method computes the cost of the cheapest path from top-left corner of the field to the bottom-right corner.
     //The only possible moves are right and down.
-    int PathCost() 
+    int PathCost();
     
  };
+
+#endif
 
 
