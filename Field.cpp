4eca60a8e67b5f8ad12912ae735af080484521b1
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-11-20 18:01:45 -0500

    Update Field.cpp

--- Field.cpp
+++ Field.cpp
@@ -69,6 +69,9 @@ int Field::Weight(int x1, int y1, int x2, int y2)
 {
      rows = matirx.size();
      cols = matirx[0].size();
+     if(x1<0 || x2<0 || y1<0 || y2<0 || x1>=cols || x2>=cols || y1>=rows ||y2>=rows)
+         throw out_of_range("Index is out of range");  // do if coordinates go beyond the array boundaries
+    
      if((0 <= x1 < cols) && (0 <= x2 < cols) && (0 <= y1 < rows) && (0 <= y2 < rows))
      {
         //remember the normal coordinate graph is Right to Up, here is Right to down.
@@ -88,9 +91,9 @@ int Field::Weight(int x1, int y1, int x2, int y2)
              return precompSum[rows2][cols2] - precompSum[rows2][cols1-1] - precompSum[rows1-1][cols2] + precompSum[rows1-1][cols1-1];         
      }
     
-     else
+     /**else
         throw out_of_range("Index is out of range");  // do if coordinates go beyond the array boundaries
-    
+    **/
 }
 
 //This method computes the cost of the cheapest path from top-left corner of the field to the bottom-right corner.
