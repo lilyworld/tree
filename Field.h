2bacb6379f7fe708d44fc33bbf1ac58eba9705a4
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-11-20 00:12:56 -0500

    Update Field.h

--- Field.h
+++ Field.h
@@ -8,11 +8,19 @@ class Field
   public:
     Field(const vector<vector<int>> & element);
     Field(vector<vector<int>> && element);
+  
+    /**@returns the sum of all elements in the rectangle defined by the points (x1,y1) and (x2,y2). **/
+    //need to do the precomputing in the constructors
     int Weight(int x1, int y1, int x2, int y2);
     
     //This method computes the cost of the cheapest path from top-left corner of the field to the bottom-right corner.
     //The only possible moves are right and down.
     int PathCost();
+  
+  private:
+    int rows, cols;   //numbers of rows and cols
+    vector<vector<int>> matirx;   // store the 2-D matirx
+    vector<vector<int>> precompSum;  // store the precomputing sum
     
  };
 
