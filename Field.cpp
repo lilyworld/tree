0343b1fc6444b91e9ce052fab7887a51c917bf59
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-11-20 18:29:51 -0500

    Update Field.cpp

--- Field.cpp
+++ Field.cpp
@@ -67,7 +67,7 @@ Field::Field(vector<vector<int>> && element)
  //need to do the precomputing in the constructors
 int Field::Weight(int x1, int y1, int x2, int y2)
 {
-     int weight=0;
+     int weight =0;
      rows = matirx.size();
      cols = matirx[0].size();
      if(x1<0 || x2<0 || y1<0 || y2<0 || x1>=cols || x2>=cols || y1>=rows ||y2>=rows)
@@ -80,15 +80,13 @@ int Field::Weight(int x1, int y1, int x2, int y2)
         int rows2 = max(y1, y2);
         int cols1 = min(x1, x2);
         int cols2 = max(x1, x2);
-         
+        weight = precompSum[rows2][cols2];
         // Weight(1,1,1,1)=6-2-3+1=2; Weight(1,1,2,2)=18-3-6+1=10
-         if(rows1 == 0 && cols1 ==0)
-             weight=precompSum[rows2][cols2];
-         if(cols1 == 0)
+         if(rows1 >= 1)
              weight= precompSum[rows2][cols2] - precompSum[rows1-1][cols2];
-         else if(rows1 == 0)
+         else if(cols >= 1)
              weight= precompSum[rows2][cols2] - precompSum[rows2][cols1-1];
-         else
+         else if(rows1 >= 1 && cols >= 1)
              weight= precompSum[rows2][cols2] - precompSum[rows2][cols1-1] - precompSum[rows1-1][cols2] + precompSum[rows1-1][cols1-1];         
      }
     
