3caacc2af68bc7694fade039c600ce9f354aebc7
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-11-20 19:10:24 -0500

    Update Field.cpp

--- Field.cpp
+++ Field.cpp
@@ -82,11 +82,11 @@ int Field::Weight(int x1, int y1, int x2, int y2)
         int cols2 = max(x1, x2);
         weight = precompSum[rows2][cols2];
         // Weight(1,1,1,1)=6-2-3+1=2; Weight(1,1,2,2)=18-3-6+1=10
-         if(rows1 >= 1)
+         if(row1 >= 1)
              weight= precompSum[rows2][cols2] - precompSum[rows1-1][cols2];
-         else if(cols >= 1)
+         else if(cols1 >= 1)
              weight= precompSum[rows2][cols2] - precompSum[rows2][cols1-1];
-         else if(rows1 >= 1 && cols >= 1)
+         else if(rows1 >= 1 && cols1 >= 1)
              weight= precompSum[rows2][cols2] - precompSum[rows2][cols1-1] - precompSum[rows1-1][cols2] + precompSum[rows1-1][cols1-1];         
      }
     
