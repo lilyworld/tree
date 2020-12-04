bba2a25af7152618d2f06bbceab84f7a693250d1
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-12-03 21:32:03 -0500

    Update AudioProcessor.h

--- AudioProcessor.h
+++ AudioProcessor.h
@@ -1,8 +1,24 @@
+#ifndef AUDIOPROCESSOR_H
+#define	AUDIOPROCESSOR_H
 #include <iostream>
-
-
+#incldue <vector>
 
 class AudioProcessor
 {
   pubilc:
+    std::vector Compress(const std::vector & audio, short threshold, float rate);
+    std::vector CutOutSilence(const std::vector & audio, short level, int silenceLength);
+    std::vector StretchTwice(const std::vector & audio);
+    std::vector Normalize(const std::vector & audio, short normalizeTarget);
+    
+  
+  
+  
+  
+  
+  
+  
+};
+
+#endif
     
