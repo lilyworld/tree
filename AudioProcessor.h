da1c7aaf7384f46fc69139b8101a3e3332ce9148
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-12-03 22:42:24 -0500

    Update AudioProcessor.h

--- AudioProcessor.h
+++ AudioProcessor.h
@@ -6,17 +6,25 @@
 class AudioProcessor
 {
   pubilc:
-    std::vector Compress(const std::vector & audio, short threshold, float rate);
+    /**@@returns the compressed version of the given audio. **/
+    //“Compressed” means that any number from audio that is larger than threshold or smaller
+    // than -threshold has it’s “above the threshold” portion reduced rate time.
+    std::vector Compress(const std::vector & audio, short threshold, float rate); 
+  
+    /**@@returns the version of audio where all silences of at least silenceLength length are removed.**/
     std::vector CutOutSilence(const std::vector & audio, short level, int silenceLength);
+  
+    /**@@Returns the version of audio where between each pair of original samples from audio an extra sample appears.**/
     std::vector StretchTwice(const std::vector & audio);
+  
+    /**@@returns a version of audio where all samples are proportionally increases or decreased to ensure that the
+    largest absolute value from all resulting samples is exactly normalizeTarget. **/
     std::vector Normalize(const std::vector & audio, short normalizeTarget);
     
   
-  
-  
-  
-  
-  
+  private:
+    float numsamp;  //numbers/samples.
+    vector<int> CompAudio; //use to store new values.
   
 };
 
