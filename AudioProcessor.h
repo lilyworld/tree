37e752d84a226afaddc79242b90eff01e6c1e582
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-12-03 23:08:39 -0500

    Update AudioProcessor.h

--- AudioProcessor.h
+++ AudioProcessor.h
@@ -9,22 +9,22 @@ class AudioProcessor
     /**@@returns the compressed version of the given audio. **/
     //“Compressed” means that any number from audio that is larger than threshold or smaller
     // than -threshold has it’s “above the threshold” portion reduced rate time.
-    std::vector Compress(const std::vector & audio, short threshold, float rate); 
+    std::vector Compress(const std::vector<short>& audio, short threshold, float rate); 
   
     /**@@returns the version of audio where all silences of at least silenceLength length are removed.**/
-    std::vector CutOutSilence(const std::vector & audio, short level, int silenceLength);
+    std::vector CutOutSilence(const std::vector<short>& audio, short level, int silenceLength);
   
     /**@@Returns the version of audio where between each pair of original samples from audio an extra sample appears.**/
-    std::vector StretchTwice(const std::vector & audio);
+    std::vector StretchTwice(const std::vector<short>& audio);
   
     /**@@returns a version of audio where all samples are proportionally increases or decreased to ensure that the
     largest absolute value from all resulting samples is exactly normalizeTarget. **/
-    std::vector Normalize(const std::vector & audio, short normalizeTarget);
+    std::vector Normalize(const std::vector<short>& audio, short normalizeTarget);
     
   
   private:
     float numsamp;  //numbers/samples.
-    vector<int> CompAudio; //use to store new values.
+    vector<short> CompAudio; //use to store new values.
   
 };
 
