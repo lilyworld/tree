ea415aa8c830e94e276895aeb6b5f78b888fd21f
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-12-07 23:07:53 -0500

    Update AudioProcessor.h

--- AudioProcessor.h
+++ AudioProcessor.h
@@ -9,17 +9,17 @@ class AudioProcessor
     /**@@returns the compressed version of the given audio. **/
     //“Compressed” means that any number from audio that is larger than threshold or smaller
     // than -threshold has it’s “above the threshold” portion reduced rate time.
-    std::vector Compress(const std::vector<short>& audio, short threshold, float rate); 
+    std::vector<short> Compress(const std::vector<short>& audio, short threshold, float rate); 
   
     /**@@returns the version of audio where all silences of at least silenceLength length are removed.**/
-    std::vector CutOutSilence(const std::vector<short>& audio, short level, int silenceLength);
+    std::vector<short> CutOutSilence(const std::vector<short>& audio, short level, int silenceLength);
   
     /**@@Returns the version of audio where between each pair of original samples from audio an extra sample appears.**/
-    std::vector StretchTwice(const std::vector<short>& audio);
+    std::vector<short> StretchTwice(const std::vector<short>& audio);
   
     /**@@returns a version of audio where all samples are proportionally increases or decreased to ensure that the
     largest absolute value from all resulting samples is exactly normalizeTarget. **/
-    std::vector Normalize(const std::vector<short>& audio, short normalizeTarget);
+    std::vector<short> Normalize(const std::vector<short>& audio, short normalizeTarget);
     
   
   private:
