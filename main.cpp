1658a12c0196207a5403dd196528e73881f819e4
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-12-08 20:59:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,39 @@
 #include "AudioProcessor.cpp"
 using namespace std;
 
+void TestCutOutSilence()
+{
+    AudioProcessor aProc;
+
+    // INPUT 1
+    std::vector<short> input{ 1, 2, 5, 6, 10, 0, -2, -5, -5, -8 };
+    std::vector<short> expected{ 1, 2, 5, 6, 10, -8 };
+    auto output{ aProc.CutOutSilence(input, 5, 4) };
+
+    if (output != expected)
+        std::cout << "bad CutOutSilence! failed on input 1." << std::endl;
+
+    // INPUT 2
+    input = { 1, 2, 5, 6, -4, 4, 10, 0, -2, -5, -4};
+    expected = { 6, -4, 4, 10 };
+    output = aProc.CutOutSilence(input, 5, 3);
+
+    if (output != expected)
+        std::cout << "bad CutOutSilence! failed on input 2." << std::endl;
+
+    // INPUT 3
+    input = { 1, 2, 5, 6, -4, 4, -2, -5, 10, 0, -4 };
+    expected = { 1, 2, 5, 6, 10, 0, -4 };
+    output = aProc.CutOutSilence(input, 5, 4);
+
+    if (output != expected)
+        std::cout << "bad CutOutSilence! failed on input 3." << std::endl;
+}
+
 int main()
 {
-    AudioProcessor  processor;
 
-    vector<short> input{ 1, 2, 5, 6, -4, 4, 10, 0, -2, -5, -4}; 
-    vector<short> output;  
-    short y=5;
-    int x=3;
-
-    output=processor.CutOutSilence(input, y, x);
-    vector<short> expected = {6, -4, 4, 10 };
-    if (output != expected)
-        cout << "bad CutOutSilence! failed on input 3." << endl;
-    if(output == expected)
-        cout << "correct!" << endl;
-    
+    TestCutOutSilence();
     return 0;
 
 }
