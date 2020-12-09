a1cff455eb880cf7fc4a13ccd35479810f44d72f
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-12-08 21:45:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,21 +18,21 @@ void TestCutOutSilence()
         cout << "correct!"<<endl;
 
     // INPUT 2
-    input = { 1, 2, 5, 6, -4, 4, 10, 0, -2, -5, -4};
-    expected = { 6, -4, 4, 10 };
-    output = aProc.CutOutSilence(input, 5, 3);
+    vector<short> input2 = { 1, 2, 5, 6, -4, 4, 10, 0, -2, -5, -4};
+    vector<short> expected2 = { 6, -4, 4, 10 };
+    output2 = aProc.CutOutSilence(input2, 5, 3);
 
-    if (output != expected)
+    if (output2 != expected2)
         std::cout << "bad CutOutSilence! failed on input 2." << std::endl;
      else
         cout << "correct!"<<endl;
 
     // INPUT 3
-    input = { 1, 2, 5, 6, -4, 4, -2, -5, 10, 0, -4 };
-    expected = { 1, 2, 5, 6, 10, 0, -4 };
-    output = aProc.CutOutSilence(input, 5, 4);
+    vector<short> input3 = { 1, 2, 5, 6, -4, 4, -2, -5, 10, 0, -4 };
+    vector<short> expected3 = { 1, 2, 5, 6, 10, 0, -4 };
+    output3 = aProc.CutOutSilence(input3, 5, 4);
 
-    if (output != expected)
+    if (output3 != expected3)
         std::cout << "bad CutOutSilence! failed on input 3." << std::endl;
      else
         cout << "correct!"<<endl;
