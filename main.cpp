f83f16d66e5407bc442011a1270394eab22ee07c
lilyworld <56894410+lilyworld@users.noreply.github.com>
2020-12-08 20:52:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,14 +13,12 @@ int main()
     int x=4;
 
     output=processor.CutOutSilence(input, y, x);
-    /**for(int i=0; i<compressedAudio.size(); i++)
-    {
-        cout << compressedAudio[i]<< ", ";
-    }**/
-    //couinput = { 1, 2, 5, 6, -4, 4, -2, -5, 10, 0, -4 };
     vector<short> expected = { 1, 2, 5, 6, 10, 0, -4 };
     if (output != expected)
-        std::cout << "bad CutOutSilence! failed on input 3." << std::endl;
+        cout << "bad CutOutSilence! failed on input 3." << endl;
+    if(output == expected)
+        cout << "correct!" << endl;
+    
     return 0;
 
 }
